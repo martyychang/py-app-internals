1fb43d9cac781eb853bcd0c0845d7df2dd37f70a
Ubuntu <develop@markable.us>
2016-11-08 02:33:37 +0000

    Prove coroutine logging

--- src/app/internals.py
+++ src/app/internals.py
@@ -3,11 +3,12 @@ import logging
 
 # Configure logging
 FORMAT = '|'.join([
-        '%(asctime)-15s',
-        '%(levelname)s',
-        '%(name)s',
-        '%(message)s',
-        ])
+    '%(asctime)-15s',
+    '%(levelname)s',
+    '%(name)s',
+    '%(funcName)s',
+    '%(message)s',
+])
 
 logging.basicConfig(format=FORMAT, level=logging.INFO)
 
--- src/tornado.demo.py
+++ src/tornado.demo.py
@@ -1,5 +1,6 @@
 import tornado.ioloop
 import tornado.web
+import tornado.gen as gen
 
 from app.internals import logger
 
@@ -13,9 +14,24 @@ class MainHandler(tornado.web.RequestHandler):
         self.write('Tornado {} says, "Hello!"'.format(tornado.version))
 
 
+class ApiHandler(tornado.web.RequestHandler):
+    @gen.coroutine
+    def get(self):
+        _logger.info('ApiHandler.get')
+        result = yield do_api_get()
+        self.write(result)
+
+
+@gen.coroutine
+def do_api_get():
+    _logger.info('somewhere inside a coroutine...')
+    raise gen.Return('gotten')
+
+
 if __name__ == '__main__':
     application = tornado.web.Application([
         (r'/', MainHandler),
+        (r'/api', ApiHandler),
     ])
     application.listen(8888)
 
